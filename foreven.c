18ce363e63adcc9d5d499e0e9af319e0a8f9e949
JUBAIR-10 <dcarlos1833@gmail.com>
2025-04-25 22:09:09 +0600

    Add files via upload

---
+++ foreven.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+    int i, sum = 0;
+    printf("odd number :\n");
+    for (i = 1; i <=100; i -= 2)
+    {
+        sum += i;
+    }
+    printf("Sum = %d\n", sum);
+    return 0;
+}
